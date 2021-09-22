a21ab7b5671eb9a5a1fe902762c98d60bd2f9807
viktor17 <89130897+therealking17@users.noreply.github.com>
2021-09-22 09:33:30 +0200

    Add files via upload
    generate random number

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Zufallszahl_generieren
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int genRand = r.Next(1, 100);
+            Console.WriteLine("Erraten sie eine Zahl zwischen 1 und 100");
+            // string ui = Console.ReadLine();
+            // int guess = Convert.ToInt16(ui);
+            bool correct = true;
+            int V = 1;
+            while (correct)
+            {
+                try
+                {
+                    string guessString = Console.ReadLine();
+                    int guess = Convert.ToInt32(guessString);
+                    if (guess < genRand)
+                    {
+                        Console.WriteLine("Zu klein");
+                        Console.WriteLine("--------------------");
+                        Console.WriteLine("Geben sie eine Zahl von 1-100");
+                        V++;
+                    }
+                    else if (guess > genRand)
+                    {
+                        Console.WriteLine("Zu gross");
+                        Console.WriteLine("--------------------");
+                        Console.WriteLine("Geben sie eine Zahl von 1-100");
+                        V++;
+                    }
+
+                    else if (guess == genRand)
+                    {
+
+                        //correct = true;
+                        Console.WriteLine("Sie haben die richtige Zahl erraten!");
+                        Console.WriteLine("Sie haben " + V + " Versuche gebraucht");
+                    }
+                }
+                catch (Exception)
+                {
+
+                    Console.WriteLine("Geben sie bitte eine Zahl zwischen 1 und 100 ein: ");
+                }
+              
+          
+            }
+
+        }
+
+
+
+    }
+}
